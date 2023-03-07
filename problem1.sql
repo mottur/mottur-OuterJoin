b48c5d99275f68a26f2f1c40cb9713c283fda082
Meghana Ottur <meghana.ottur@gmail.com>
2023-03-07 02:00:10 +0000

    complete

---
+++ problem1.sql
@@ -0,0 +1,2 @@
+SELECT * FROM class LEFT JOIN student
+ON class.class_title = student.class_title;
---
+++ problem2.sql
@@ -0,0 +1,2 @@
+SELECT * FROM class RIGHT JOIN textbook
+ON class.class_title = textbook.class_title;
